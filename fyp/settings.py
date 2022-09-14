ef78d835e2a8b62f8cec55295baafbcebc1d0bf0
Ahmad062 <86141799+Ahmad062@users.noreply.github.com>
2022-09-14 10:35:34 +0500

    Update settings.py

--- fyp/settings.py
+++ fyp/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-(4l_pj(2z_ux-lt6o5k6toyfro#5_z_#x+r(7c+7p$h=2-jciy
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1', 'covidsopschecker.herokuapp.com']
 
 
 # Application definition
